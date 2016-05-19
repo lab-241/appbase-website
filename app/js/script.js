1d042385e539e00b709ca0945112100ff95de1f5
Michaël P.O <mike.pambo@gmail.com>
2016-05-19 14:55:28 +0200

    Add anlytics support

--- app/js/script.js
+++ app/js/script.js
@@ -375,6 +375,19 @@ var appMaster = {
     /* ----------------------------------------------------------- */
     fancybox: function(){
       $(".fancybox").fancybox();
+    }, 
+    
+    /* ----------------------------------------------------------- */
+    /*  GOOGLE ANALITICS
+    /* ----------------------------------------------------------- */
+    analytics: function(){
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+    
+      ga('create', 'UA-77972960-1', 'auto');
+      ga('send', 'pageview');
     }
 };
 
@@ -382,4 +395,5 @@ var appMaster = {
 $(document).ready(function() {
   appMaster.scollToTop();
   appMaster.slick();
+  appMaster.analytics();
 });
